a6814cd5bece36e92d17bb636b2587f8d07db6e9
zhangbo <mageboer@qq.com>
2024-08-27 16:18:43 +0800

    添加event model method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
@@ -11,26 +12,33 @@
 
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import ToDoForm from "./components/ToDoForm";
 import uniqueId from "lodash.uniqueid";
 
 export default {
   name: "app",
   components: {
     ToDoItem,
+    ToDoForm,
   },
   data() {
     return {
       ToDoItems: [
-        {id: uniqueId("todo-"), label: "Learn Vue", done: false},
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
         {
           id: uniqueId("todo-"),
           label: "Create a Vue project with the CLI",
           done: true,
         },
-        {id: uniqueId("todo-"), label: "Have fun", done: true},
-        {id: uniqueId("todo-"), label: "Create a to-do list", done: false},
+        { id: uniqueId("todo-"), label: "Have fun", done: true },
+        { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
       ],
     };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+    },
   },
 };
 </script>
